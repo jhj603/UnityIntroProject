8921f4d761e9d78c756d0e572aacbad08be60a01
jhj603 <82034869+jhj603@users.noreply.github.com>
2024-05-11 12:02:35 +0900

    [feat]CameraFollowPlayer

---
+++ Assets/Scripts/Entities/Behaviours/IntroAimRotation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class IntroAimRotation : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private IntroController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<IntroController>();
+    }
+
+    private void Start()
+    {
+        controller.OnLookEvent += OnAim;
+    }
+
+    private void OnAim(Vector2 direction)
+    {
+        RotateCharacter(direction);
+    }
+
+    private void RotateCharacter(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+    }
+}
