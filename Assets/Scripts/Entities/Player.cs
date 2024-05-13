0f648827393772348fdf9fa93a1ba5ffbc77718a
jhj603 <82034869+jhj603@users.noreply.github.com>
2024-05-13 13:31:56 +0900

    [feat]ShowTime

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -7,14 +7,19 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private TMP_Text text;
     [SerializeField] private List<Sprite> sprites = new List<Sprite>((int)CharacterType.Knight);
+    [SerializeField] private Vector3 camOffset;
 
     private CharacterStatHandler characterStatHandler;
     private SpriteRenderer spriteRenderer;
+
+    private Camera camera;
 
     private void Awake()
     {
         characterStatHandler = GetComponent<CharacterStatHandler>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        camera = Camera.main;
     }
 
     // Start is called before the first frame update
@@ -30,5 +35,10 @@ public class Player : MonoBehaviour
             case CharacterType.Knight:
                 break;
         }
+    }
+
+    private void LateUpdate()
+    {
+        camera.transform.position = transform.position + camOffset;
     }
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,22 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    [SerializeField] private CharacterStatHandler characterStatHandler;
+    [SerializeField] private GameObject Player;
+    [SerializeField] private GameObject NPC;
+    [SerializeField] private TMP_Text timeTxt;
+
+    private CharacterStatHandler characterStatHandler;
 
     private void Awake()
     {
         if (null == Instance)
             Instance = this;
+    }
+
+    private void Start()
+    {
+        Instantiate(Player);
+
+        characterStatHandler = Player.GetComponent<CharacterStatHandler>();
 
         if (PlayerPrefs.HasKey("playerName"))
         {
             characterStatHandler.CurrentStat.name = PlayerPrefs.GetString("playerName");
             characterStatHandler.CurrentStat.characterType = (CharacterType)PlayerPrefs.GetInt("playerType");
         }
+    }
+
+    private void Update()
+    {
+        timeTxt.text = DateTime.Now.ToString(("HH:mm"));
     }
 }
